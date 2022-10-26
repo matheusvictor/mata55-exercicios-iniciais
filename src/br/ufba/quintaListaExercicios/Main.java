8336ae358f81952b7822ebde64d8bd4eb4cd3641
Matheus Victor <matheusvictor.salles@gmail.com>
2022-10-26 16:42:35 -0300

    feat: Cria instâncias dos objetos

--- src/br/ufba/quintaListaExercicios/Main.java
+++ src/br/ufba/quintaListaExercicios/Main.java
@@ -3,17 +3,23 @@ package br.ufba.quintaListaExercicios;
 public class Main {
 
     public static void main(String[] args) {
-        // Retangulo r = new Retangulo(2, 2);
-        // System.out.println(r.calcularArea());
-        // System.out.println(r.calcularPerimetro());
 
-        Triangulo t = new Triangulo(2, 2);
-        System.out.println(t.calcularArea());
-        System.out.println(t.calcularPerimetro());
+//        Retangulo r = new Retangulo(2, 2);
+//        System.out.println(r.calcularArea());
+//        System.out.println(r.calcularPerimetro());
 
-        Circulo c = new Circulo(2, 2, 4);
-        System.out.println(c.calcularArea());
-        System.out.println(c.calcularPerimetro());
+//        Triangulo trianguloIsoceles = new TrianguloIsoceles(8, 15);
+//        System.out.println(trianguloIsoceles.calcularArea());
+//        System.out.println(trianguloIsoceles.calcularPerimetro());
+
+        TrianguloRetangulo trianguloRetangulo = new TrianguloRetangulo(15, 8);
+        System.out.println(trianguloRetangulo.calcularPerimetro());
+        System.out.println(trianguloRetangulo.getHipotenusa());
+
+//        Circulo c = new Circulo(2, 2, 4);
+//        System.out.println(c.getNumeroLados());
+//        System.out.println(c.calcularArea());
+//        System.out.println(c.calcularPerimetro());
     }
 
 }
